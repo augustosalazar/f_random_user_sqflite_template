471bc89242f58152ce3cffd38cd23b371a2cbcdd
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2021-09-22 21:55:58 -0500

    Add two more cases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,132 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:f_local_database_sqlite_template/data/models/user_model.dart';
+import 'package:f_local_database_sqlite_template/domain/home_controller.dart';
+import 'package:f_local_database_sqlite_template/domain/user_controller.dart';
+import 'package:f_local_database_sqlite_template/ui/pages/user_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
 
-import 'package:f_local_database_sqlite_template/main.dart';
+class MockHomeController extends GetxService
+    with Mock
+    implements HomeController {
+  var _connection = false.obs;
+  @override
+  Future onInit() {
+    super.onInit();
+    return Future.value(true);
+  }
+
+  @override
+  bool get connection => _connection.value;
+}
+
+class MockHomeControllerTrue extends GetxService
+    with Mock
+    implements HomeController {
+  var _connection = true.obs;
+  @override
+  Future onInit() {
+    super.onInit();
+    return Future.value(true);
+  }
+
+  @override
+  bool get connection => _connection.value;
+}
+
+class MockUserController extends GetxService
+    with Mock
+    implements UserController {
+  var _users = <UserModel>[].obs;
+
+  @override
+  List<UserModel> get users => _users;
+
+  @override
+  Future onInit() {
+    super.onInit();
+    return Future.value(true);
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('No connection', (WidgetTester tester) async {
+    final MockHomeController _mockHome = MockHomeController();
+    Get.put<HomeController>(_mockHome);
+
+    final MockUserController _mockUser = MockUserController();
+    Get.put<UserController>(_mockUser);
+
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    WidgetsFlutterBinding.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      MaterialApp(
+        home: MaterialApp(
+          home: UserListPage(),
+        ),
+      ),
+    );
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byKey(Key('addUserButton')), findsOneWidget);
+
+    await tester.tap(find.byKey(Key('addUserButton')));
+
+    await tester.pump();
+
+    await tester.pump();
+
+    expect(find.byIcon(Icons.portable_wifi_off_rounded), findsOneWidget);
+  });
+
+  testWidgets('Connection ok', (WidgetTester tester) async {
+    final MockHomeControllerTrue _mockHome = MockHomeControllerTrue();
+    Get.put<HomeController>(_mockHome);
+
+    final MockUserController _mockUser = MockUserController();
+    Get.put<UserController>(_mockUser);
+
+    // Build our app and trigger a frame.
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      MaterialApp(
+        home: MaterialApp(
+          home: UserListPage(),
+        ),
+      ),
+    );
+    await tester.pump();
+
+    expect(find.byKey(Key('addUserButton')), findsOneWidget);
+
+    await tester.tap(find.byKey(Key('addUserButton')));
+
+    await tester.pump();
+
+    await tester.pump();
+
+    expect(find.byIcon(Icons.add), findsOneWidget);
+  });
+
+  testWidgets('Add one, delete with swipe', (WidgetTester tester) async {
+    //add one
+    //verify
+    //delete wil swipe
+  });
+
+  testWidgets('Add one, delete all', (WidgetTester tester) async {
+    //add one
+    //add one
+    //verify
+    // delete all
+    // verify
   });
 }
