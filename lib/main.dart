4993f50b990705a9a6ab0fb6038875a4c1671518
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-04-28 10:21:34 -0500

    include controllers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:f_local_database_sqlite_template/ui/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:loggy/loggy.dart';
+import 'core/network_info.dart';
+import 'domain/repositories/user_repository.dart';
+import 'domain/use_case/users.dart';
 import 'ui/home.dart';
 
 void main() async {
@@ -10,5 +16,11 @@ void main() async {
     ),
   );
 
+  Get.put(Connectivity());
+  Connectivity c = Get.find();
+  Get.put(NetworkInfo(connectivity: c));
+  Get.put(UserRepository());
+  Get.put(Users());
+  Get.put(HomeController());
   runApp(Home());
 }
